fd81bf8fcbdd87e6444a6948befca9cfff3e907d
Prabhacod <prabha29070607@gmail.com>
2026-06-23 17:56:43 +0530

    Build Streamlit dashboard for predictions

---
+++ app.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import joblib
+import pandas as pd
+model = joblib.load("models/model.pkl")
+st.title("Predictive Maintenance System")
+values = []
+for i in range(1,22):
+    values.append(
+        st.number_input(f"Sensor {i}", value=0.0)
+    )
+if st.button("Predict"):
+    data = pd.DataFrame([values])
+    prediction = model.predict(data)[0]
+    if prediction == 1:
+        st.error("Maintenance Required")
+    else:
+        st.success("Machine Healthy")
